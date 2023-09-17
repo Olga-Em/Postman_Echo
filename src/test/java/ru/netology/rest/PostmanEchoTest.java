4230c5a9b220ad18ac81a3ed05cb22d7611091e2
Olga Em <olga.em020@gmail.com>
2023-09-17 22:21:45 +0700

    Test fail

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then().log().all()
                 .statusCode(200)
-                .body("data", equalTo("hi"))
+                .body("data", equalTo("ok"))
         ;
     }
 }
